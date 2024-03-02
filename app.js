f12b8d121a12ef11d9f7d2c2ac69e7d0ed9d61c2
yusufalp <alpyusuf007@gmail.com>
2024-03-02 11:58:16 -0800

    build passport session

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
+require("./config/connection");
+require("./config/authStrategies");
 
 const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("node:path");
 
+const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const userAvailabilityRoutes = require("./routes/userAvailabilityRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -19,9 +24,33 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SECRET_KEY,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/userAvailability", userAvailabilityRoutes);
 app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("Error occurred:", err);
+
+  if (err.code === 11000) {
+    return res
+      .status(400)
+      .json({ error: { message: "Already have an account? Try logging in." } });
+  }
+
+  res.status(500).json({ success: { message: err.message } });
+});
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Come one, come all...</h1>");
