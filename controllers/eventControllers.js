342c1212facde02b08f7af0beccf2c267542c4cf
yusufalp <alpyusuf007@gmail.com>
2024-03-11 06:24:25 -0700

    build event controller and update event routes

---
+++ controllers/eventControllers.js
@@ -0,0 +1,210 @@
+const Event = require("../models/eventModel");
+
+const getAllEvents = async (req, res, next) => {
+  try {
+    const events = await Event.find({});
+
+    res.status(200).json({
+      success: { message: "All events found!" },
+      data: events,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getEventById = async (req, res, next) => {
+  const { eventId } = req.params;
+
+  if (!eventId) {
+    return res.status(400).json({
+      error: { message: "Event id is required!" },
+    });
+  }
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(400).json({
+        error: { message: "There is no event found!" },
+      });
+    }
+
+    res.status(200).json({
+      success: { message: "Event found!" },
+      data: event,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllEventsByOwnerId = async (req, res, next) => {
+  const { ownerId } = req.params;
+
+  if (!ownerId) {
+    return res.status(200).json({
+      error: { message: "Owner id is required!" },
+    });
+  }
+
+  try {
+    const events = await Event.find({ owner: ownerId });
+
+    if (!events) {
+      return res.status(400).json({
+        error: { message: "Error while finding events by owner!" },
+      });
+    }
+
+    res.status(200).json({
+      success: { message: "Events found!" },
+      data: events,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createEvent = async (req, res, next) => {
+  const { title, start, end, ownerId } = req.body;
+
+  if (!ownerId) {
+    return res.status(400).json({
+      error: { message: "Owner id is required" },
+    });
+  }
+
+  if (!title || !start || !end) {
+    return res.status(400).json({
+      error: { message: "All filed are required!" },
+    });
+  }
+
+  try {
+    const newEvent = new Event({
+      title,
+      dateRange: {
+        start,
+        end,
+      },
+      owner: ownerId,
+      participants: [ownerId],
+    });
+
+    await newEvent.save();
+
+    res.status(201).json({
+      success: { message: "Event is created!" },
+      data: newEvent,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateEventTitle = async (req, res, next) => {
+  const { eventId } = req.params;
+  const { title } = req.body;
+
+  if (!eventId) {
+    return res.status(400).json({
+      error: { message: "Event id is required!" },
+    });
+  }
+
+  if (!title) {
+    return res.status(400).json({
+      error: { message: "Title is required!" },
+    });
+  }
+
+  try {
+    const event = await Event.findByIdAndUpdate(
+      eventId,
+      { title },
+      { new: true }
+    );
+    if (!event) {
+      return res.status(400).json({
+        error: { message: "There is no event with this id!" },
+      });
+    }
+
+    res.status(200).json({
+      success: { message: "Event title is updated!" },
+      data: event,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateEventParticipants = async (req, res, next) => {
+  const { eventId } = req.params;
+  const { userId } = req.body;
+
+  if (!eventId) {
+    return res.status(400).json({
+      error: { message: "Event id is required!" },
+    });
+  }
+
+  if (!userId) {
+    return res.status(400).json({
+      error: { message: "User id is required!" },
+    });
+  }
+
+  try {
+    const event = await Event.findByIdAndUpdate(
+      eventId,
+      { $addToSet: { participants: userId } },
+      { new: true }
+    );
+
+    if (!event) {
+      return res.status(400).json({
+        error: { message: "There is no event with this id!" },
+      });
+    }
+
+    res.status(200).json({
+      success: { message: "Participant is added to the event!" },
+      data: event,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteEvent = async (req, res, next) => {
+  const { eventId } = req.params;
+
+  if (!eventId) {
+    return res.status(400).json({
+      error: { message: "Event id is required!" },
+    });
+  }
+
+  try {
+    await Event.findByIdAndDelete(eventId);
+
+    res.status(204).json({
+      success: { message: "Event is deleted!" },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllEvents,
+  getEventById,
+  getAllEventsByOwnerId,
+  createEvent,
+  updateEventTitle,
+  updateEventParticipants,
+  deleteEvent,
+};
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,25 +1,26 @@
 const express = require("express");
 
+const {
+  getAllEvents,
+  getEventById,
+  getAllEventsByOwnerId,
+  createEvent,
+  updateEventTitle,
+  updateEventParticipants,
+  deleteEvent,
+} = require("../controllers/eventControllers");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => res.send("<p>Get all events</p>"));
-router.get("/:eventId", (req, res, next) =>
-  res.send("<p>Get an event by id</p>")
-);
-router.get("/owner/:ownerId", (req, res, next) =>
-  res.send("<p>Get all events by owner id</p>")
-);
+router.get("/", getAllEvents);
+router.get("/:eventId", getEventById);
+router.get("/owner/:ownerId", getAllEventsByOwnerId);
 
-router.post("/", (req, res, next) => res.send("<p>Create a new even</p>"));
-router.post("/:eventId/title", (req, res, next) =>
-  res.send("<p>Find an event by id and update title</p>")
-);
-router.post("/:eventId/participants", (req, res, next) =>
-  res.send("<p>Find an event by id and update the participants list</p>")
-);
+router.post("/", createEvent);
 
-router.delete("/:eventId", (req, res, next) =>
-  res.send("<p>Delete an event</p>")
-);
+router.put("/:eventId/title", updateEventTitle);
+router.put("/:eventId/participants", updateEventParticipants);
+
+router.delete("/:eventId", deleteEvent);
 
 module.exports = router;
