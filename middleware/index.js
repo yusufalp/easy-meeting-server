823ba76e9168386c8685bc4f22b0317c031fec8c
yusufalp <alpyusuf007@gmail.com>
2024-05-21 23:46:21 -0700

    built JWT authentication middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated) {
     return next();
@@ -8,4 +10,25 @@ const isAuthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { isAuthenticated };
+const authenticateJWT = (req, res, next) => {
+  const JWT_SECRET =
+    process.env.JWT_SECRET_KEY || "i-hope-i-will-not-need-this";
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, JWT_SECRET, (error, user) => {
+      if (error) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+};
+
+module.exports = { isAuthenticated, authenticateJWT };
